843e31c82a6a68ef2becddfa57cb97eb5e004b33
lokesh739 <lokeshmaheswari2003@gmail.com>
2025-06-01 13:18:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0;
+        int n=s.length();
+        while(i<n && s[i]==' '){
+            i++;
+        }
+        int sign=1;
+        if(i<n && (s[i]=='+' || s[i]=='-')){
+            sign=(s[i]=='-')?-1:1;
+            i++;
+        }
+
+        long result=0;
+        while(i<n && isdigit(s[i])){
+            result=result*10+(s[i]-'0');
+
+            if(result<INT_MIN) return INT_MIN;
+            if(result>INT_MAX) return INT_MAX;
+
+            i++;
+        }
+        return sign*result;
+    }
+};
